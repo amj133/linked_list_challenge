059e503d55a001b4b5e240b631b6936a0e85d5dc
amj133 <amj@vt.edu>
2018-05-16 09:11:26 -0600

    initial commit - test it exists and head starts as nil

---
+++ lib/linked_list.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+class LinkedList
+  attr_reader :head
+
+  def initialize
+    @head = nil
+  end
+
+end
---
+++ test/linked_list_test.rb
@@ -0,0 +1,63 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/linked_list'
+
+class LinkedListTest < Minitest::Test
+
+  def test_it_exists
+    list = LinkedList.new
+
+    assert_instance_of LinkedList, list
+  end
+
+  def test_starts_with_head_equal_to_nil
+    list = LinkedList.new
+
+    assert_nil list.head
+  end
+
+end
+
+# > require "./lib/linked_list"
+# > list = LinkedList.new
+# => <LinkedList head=nil #45678904567>
+# > list.head
+# => nil
+# > list.append(5)
+# => 5
+# > list
+# => <LinkedList head=<Node data=5 next_node=nil #5678904567890> #45678904567>
+# > list.head.next_node
+# => nil
+# > list.count
+# => 1
+# > list.to_s
+# => "5"
+# > list.append(4)
+# => 4
+# > list.head.next_node
+# => <Node data=4 next_node=nil #5678904567890>
+# > list.count
+# => 2
+# > list.to_s
+# => "54"
+# > list.prepend(3)
+# => 3
+# > list.count
+# => 3
+# > list.to_s
+# => "354"
+# > list.insert(1, 8)
+# => 8
+# > list.to_s
+# => 3854
+# > list.pop
+# => 4
+# > list.to_s
+# => 385
+# > list.shift
+# => 3
+# > list.to_s
+# => 85
+# ```
